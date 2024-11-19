586bea90e17a7852d4fe19c58c978d3f3424fa20
Karimah Azzuhdu T.M. <karimah.atm@gmail.com>
2024-11-19 03:18:02 +0000

    complete project

--- index.js
+++ index.js
@@ -24,6 +24,18 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req, res) => {
+  let thisIP = req.ip;
+  let thisLang = req.headers['accept-language'];
+  let thisSoft = req.headers['user-agent'];
+
+  res.json({
+    ipaddress: thisIP,
+    language: thisLang,
+    software: thisSoft
+  })
+})
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
